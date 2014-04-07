406419bdf6f4c294b63b71906b8ba9001703bf97
suisho <suisho000000@gmail.com>
2014-04-08 01:08:33 +0900

    to object

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
 var defaults = require("defaults")
-var GLYPH_REPLACEMENT_PLEFIX = "glyglyglygly"
-var GLYPH_ELEMENT_REPLACEMENTS = {
+
+var PseudoPseudo = function(opts){
+  var options = defaults(opts, {
+    prefix : "pseudo",
+  })
+  this.prefix = options.prefix
+
+}
+
+PseudoPseudo.prototype.elementReplacement = {
   "@" : "namespace",
   "(" : "lc",
   ")" : "r",
 }
-var GLYPH_CLASS_REPLACEMENTS = {
+PseudoPseudo.prototype.classReplacement = {
   ":" : "pseudo",
 }
 
-function replaceNot(selector){
+
+PseudoPseudo.prototype.replaceNot = function(selector){
   return selector.replace(/:not\((.+)\)/g, "__NOT__$1")
 }
-function restoreNot(selector){
+
+PseudoPseudo.prototype.restoreNot = function(selector){
   return selector.replace(/__NOT__(\S+)+/g, ":not($1)")
 }
 
-
-function replaceAsElement(glyph){
-  return  GLYPH_REPLACEMENT_PLEFIX + "__" + GLYPH_ELEMENT_REPLACEMENTS[glyph] + "___"
-}
-function replaceAsClass(glyph){
-  return ".__" + GLYPH_REPLACEMENT_PLEFIX + "__" + GLYPH_CLASS_REPLACEMENTS[glyph] + "___";
+PseudoPseudo.prototype.replaceAsElement = function(glyph){
+  return this.prefix + "__" + this.elementReplacement[glyph] + "___"
 }
 
+PseudoPseudo.prototype.replaceAsClass = function(glyph){
+  return "." + this.prefix + "__" + this.classReplacement[glyph] + "___";
+}
 
-function replaceGlyphFunc(str){
-  str = replaceNot(str)
-  for(var glyph in GLYPH_ELEMENT_REPLACEMENTS){
-    var replaced = replaceAsElement(glyph)
+PseudoPseudo.prototype.replace = function(str){
+  str = this.replaceNot(str)
+  for(var glyph in this.elementReplacement){
+    var replaced = this.replaceAsElement(glyph)
     str = str.split(glyph).join(replaced)
   }
-  for(var glyph in GLYPH_CLASS_REPLACEMENTS){
-    var replaced = replaceAsClass(glyph)
+  for(var glyph in this.classReplacement){
+    var replaced = this.replaceAsClass(glyph)
     str = str.split(glyph).join(replaced)
   }
   return str
 }
+PseudoPseudo.prototype.restore = function(str){
+  str = this.restoreNot(str)
 
-function restoreGlyphFunc(str){
-  str = restoreNot(str)
-
-  for(var glyph in GLYPH_ELEMENT_REPLACEMENTS){
-    var replaced = replaceAsElement(glyph)
-    str = str.split(replaced).join(glyph)
+  for(var glyph in this.elementReplacement){
+    var restored = this.replaceAsElement(glyph)
+    str = str.split(restored).join(glyph)
   }
-  for(var glyph in GLYPH_CLASS_REPLACEMENTS){
-    var replaced = replaceAsClass(glyph)
-    str = str.split(replaced).join(glyph)
+  for(var glyph in this.classReplacement){
+    var restored = this.replaceAsClass(glyph)
+    str = str.split(restored).join(glyph)
   }
   return str
-}
-
-var PseudoPseudo = function(opts){
-  var options = defaults(opts, {
-    prefix : "pseudo_",
-  })
-  this.prefix = options.prefix
-}
-PseudoPseudo.prototype.replace = function(selector){
-  return replaceGlyphFunc(selector)
-}
-PseudoPseudo.prototype.restore = function(selector){
-  return restoreGlyphFunc(selector)
 }
 
 module.exports = function(opts){
+
   return new PseudoPseudo(opts)
 }
--- test/index.js
+++ test/index.js
@@ -8,21 +8,23 @@ var tests = [
   'div:not(.outer) .inner',
   'audio:not([controls])'
 ]
+
+
 tests.forEach(function(selector){
   describe(selector, function(){
     it('specificity', function(){
       var dummy = pseudopseudo().replace(selector)
       var beforeSpecify = specificity.calculate(selector)
       var dummySpecify = specificity.calculate(dummy)
-      console.log(dummy)
       assert.deepEqual(
         dummySpecify[0].specificity,
         beforeSpecify[0].specificity
       )
     })
     it('restore', function(){
-      var replaced = pseudopseudo().replace(selector)
-      var restore = pseudopseudo().restore(replaced)
+      var pseudo = pseudopseudo()
+      var replaced = pseudo.replace(selector)
+      var restore = pseudo.restore(replaced)
       assert.equal(restore, selector)
     })
 
